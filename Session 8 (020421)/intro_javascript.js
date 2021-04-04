c71155c829e4de458f15a5842af81f4fac47acf4
ha.ntt <nguyenthithuha177@gmail.com>
2021-04-04 16:56:10 +0700

    session 8's homework

---
+++ Session 8 (020421)/intro_javascript.js
@@ -0,0 +1,6 @@
+function change_content() {
+    document.getElementById("change_content").innerHTML = "Đây là nội dung mới"
+}
+function change_content_2() {
+    document.getElementById("change_content").innerHTML = "Đây là nội dung đầu tiên"
+}
